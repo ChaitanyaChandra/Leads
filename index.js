bd4c8225055381c777b0442824b8f1e2c34d1f6c
ChaitanyaChandra <chay@outlook.in>
2022-03-23 16:19:19 +0530

    dynamo db working

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 const mysql = require('mysql');
 var satelize = require('satelize');
+var AWS = require("aws-sdk");
+
+let user = "chaitu"
+let password = "@123Chaitu"
+
+var Leads_table_name = "chaitu-dynamodb-nonpord-ap-south-1-dev-leads"
+var Views_table_name = "chaitu-dynamodb-nonpord-ap-south-1-dev-views"
+
+let awsConfig = {
+    "region": "ap-south-1",
+    "endpoint": "http://dynamodb.ap-south-1.amazonaws.com",
+    "accessKeyId": "AKIAZL6G3YXMXTR6AO5L",
+    "secretAccessKey": "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"
+};
+AWS.config.update(awsConfig);
+
+let docClient = new AWS.DynamoDB.DocumentClient();
 
 const app = express()
 const port = 3000
@@ -10,77 +27,192 @@ app.use(express.static('public'))
 app.use(express.static('views/public'))
 
 app.set('view engine', 'pug')
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
-const con = mysql.createConnection({
-    host: "152.70.72.91",
-    user: "root",
-    password: "@123Chaitu",
-    database: "leads_db",
-   //insecureAuth : true
-});
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
-
-app.get('/', function (req, res) {
-    res.sendFile('public/index.html', {root: __dirname})
+app.get('/', function(req, res) {
+    res.sendFile('public/index.html', {
+        root: __dirname
+    })
 })
 
-app.get('/en-us', function (req, res) {
-    res.sendFile('public/index.html', {root: __dirname})
+app.get('/en-us', function(req, res) {
+    res.sendFile('public/index.html', {
+        root: __dirname
+    })
 })
 
-app.get('/leads', function (req, res) {
-    con.query("select id, Name, Email, Company, Subject, Message, DATE_FORMAT(Created_at, '%D %M %y at %h:%i') AS created , country_code AS Country from leads ORDER BY id DESC;", function (err, result, fields) {
-        if (err) throw err;
-            res.render('leads',{title : 'all leads', items : result})
-       // console.log(result);
-    });
-})
-
-app.get('/views', function (req, res) {
-    con.query("select id, Ipaddress, country_code AS Country,  DATE_FORMAT(Created_at, '%D %M %y at %h:%i') AS created from views ORDER BY id DESC;", function (err, result, fields) {
-        if (err) throw err;
-        res.render('views',{title : 'all views', items : result})
-       // console.log(result);
-    });
-})
-
-app.post('/submit', function (req, res){
+app.post('/submit', function(req, res) {
 
     // console.log(req.body)
-
-    satelize.satelize({ip: req.body.userIp }, function (err, payload){
+    satelize.satelize({
+        ip: req.body.userIp
+    }, function(err, payload) {
         // console.log(payload)
+        var input = {
+            "Created_on": new Date().toString(),
+            "Name": req.body.name,
+            "Email": req.body.email,
+            "Company": req.body.company,
+            "Subject": req.body.subject,
+            "Message": req.body.message,
+            "Ipaddress": payload.ip,
+            "Country_code": payload.country_code,
+            "UserInfo": JSON.stringify(payload)
+        };
 
-        const sql = "INSERT INTO leads (Name, Email, Company, Subject, Message, Ipaddress, country_code, userInfo ) VALUES ('" + req.body.name + "' , '" + req.body.email + "' , '" + req.body.company + "' , '" + req.body.subject + "' , '" + req.body.message + "' , '" +  req.body.userIp + "' , '" +  payload.country_code + "' , '"  + JSON.stringify(payload) + "')"
-        con.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("1 lead record inserted");
+        var params = {
+            TableName: Leads_table_name,
+            Item: input
+        };
+        docClient.put(params, function(err, data) {
+
+            if (err) {
+                console.log("users::save::error - " + JSON.stringify(err, null, 2));
+            } else {
+                console.log("users::save::success");
+            }
         });
     })
-    res.sendFile('public/response.html', {root: __dirname})
+    res.sendFile('public/response.html', {
+        root: __dirname
+    })
 
     // res.redirect('https://end.chaitu.net/')
 })
 
-app.post('/en-us', function (req, res){
+app.post('/en-us', function(req, res) {
 
-    console.log(req.body)
+    //console.log(req.body)
 
-    satelize.satelize({ip: req.body.userIp }, function (err, payload){
-        // console.log(payload)
-        const sql = "INSERT INTO views (Ipaddress, country_code, userInfo ) VALUES ('" + req.body.userIp + "' , '" +  payload.country_code + "' , '"  + JSON.stringify(payload) + "')"
-        con.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("1 view record inserted");
+    satelize.satelize({
+        ip: req.body.userIp
+    }, function(err, payload) {
+        var input = {
+            "Created_on": new Date().toString(),
+            "Country_code": payload.country_code,
+            "Ipaddress": payload.ip,
+            "UserInfo": JSON.stringify(payload)
+        };
+
+        var params = {
+            TableName: Views_table_name,
+            Item: input
+        };
+        docClient.put(params, function(err, data) {
+
+            if (err) {
+                console.log("users::save::error - " + JSON.stringify(err, null, 2));
+            } else {
+                console.log("users::save::success");
+            }
         });
     })
-    res.sendFile('public/chaitu.html', {root: __dirname})
+    res.sendFile('public/chaitu.html', {
+        root: __dirname
+    })
 
+})
+
+app.get('/leads', function(req, res) {
+    res.sendFile('public/LeadsLogin.html', {
+        root: __dirname
+    })
+})
+
+app.post('/leads', function(req, res) {
+    if ((user == req.body.username) && (password == req.body.password)) {
+        var params = {
+            TableName: Leads_table_name,
+            // FilterExpression: "#Created_on = :user_status_val",
+            // ExpressionAttributeNames: {
+            //     "#Created_on": "Created_on",
+            // },
+            // ExpressionAttributeValues: { ":user_status_val": 'Mon Mar 21 2022 20:26:37 GMT+0530 (India Standard Time)' }
+        };
+
+        docClient.scan(params, onScan);
+        var count = 0;
+
+        function onScan(err, data) {
+            if (err) {
+                console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("Scan succeeded.");
+                data.Items.forEach(function(itemdata) {
+                    console.log("Item :", ++count, JSON.stringify(itemdata));
+                });
+                console.log(data)
+                res.render('leads', {
+                    title: 'all leads',
+                    items: data.Items
+                })
+
+                // continue scanning if we have more items
+                if (typeof data.LastEvaluatedKey != "undefined") {
+                    console.log("Scanning for more...");
+                    params.ExclusiveStartKey = data.LastEvaluatedKey;
+                    docClient.scan(params, onScan);
+                }
+            }
+        }
+    } else {
+        res.sendFile('public/LeadsLogin.html', {
+            root: __dirname
+        })
+    }
+})
+
+app.get('/views', function(req, res) {
+    res.sendFile('public/ViewsLogin.html', {
+        root: __dirname
+    })
+})
+
+app.post('/views', function(req, res) {
+    if ((user == req.body.username) && (password == req.body.password)) {
+        var params = {
+            TableName: Views_table_name,
+            // FilterExpression: "#Created_on = :user_status_val",
+            // ExpressionAttributeNames: {
+            //     "#Created_on": "Created_on",
+            // },
+            // ExpressionAttributeValues: { ":user_status_val": 'Mon Mar 21 2022 20:26:37 GMT+0530 (India Standard Time)' }
+        };
+
+
+        docClient.scan(params, onScan);
+        var count = 0;
+
+        function onScan(err, data) {
+            if (err) {
+                console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("Scan succeeded.");
+                data.Items.forEach(function(itemdata) {
+                    console.log("Item :", ++count, JSON.stringify(itemdata));
+                });
+                console.log(data.ScannedCount)
+                res.render('views', {
+                    title: 'all views',
+                    items: data.Items
+                })
+
+                // continue scanning if we have more items
+                if (typeof data.LastEvaluatedKey != "undefined") {
+                    console.log("Scanning for more...");
+                    params.ExclusiveStartKey = data.LastEvaluatedKey;
+                    docClient.scan(params, onScan);
+                }
+            }
+        }
+    } else {
+        res.sendFile('public/ViewsLogin.html', {
+            root: __dirname
+        })
+    }
 })
 
 
